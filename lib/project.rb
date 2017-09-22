c2a43d8789f45efd3729020cf55b7269db37e9fa
Riki Montgomery <mostriki820@gmail.com>
2017-09-22 16:56:01 -0700

    fix final bug in specs and pass all 18 specs

--- lib/project.rb
+++ lib/project.rb
@@ -7,15 +7,15 @@ class Project
   end
 
   def ==(another_project)
-    self.title().==(another_project.title()).&(self.id().==(another_project.id()))
+    self.title.==(another_project.title).&(self.id.==(another_project.id))
   end
 
   def self.all
     all_projects = DB.exec("SELECT * FROM projects;")
     projects = []
-    all_projects.each() do |title|
-      project = title.fetch("title")
-      id = title.fetch("id").to_i()
+    all_projects.each do |project|
+      title = project.fetch("title")
+      id = project.fetch("id").to_i
       projects.push(Project.new({:title => title, :id => id}))
     end
     projects
@@ -23,19 +23,19 @@ class Project
 
   def save
     result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
-    @id = result.first().fetch("id").to_i()
+    @id = result.first.fetch("id").to_i
   end
 
   def self.find(id)
     result = DB.exec("SELECT * FROM projects WHERE id = #{id};")
-    title = result.first().fetch("title")
+    title = result.first.fetch("title")
     Project.new({:title => title, :id => id})
   end
 
   def volunteers
     project_volunteers = []
-    volunteers = DB.exec("SELECT * FROM volunteers WHERE project_id = #{self.id()};")
-    volunteers.each() do |volunteer|
+    volunteers = DB.exec("SELECT * FROM volunteers WHERE project_id = #{self.id};")
+    volunteers.each do |volunteer|
       name = volunteer.fetch("name")
       project_id = volunteer.fetch("project_id").to_i
       id = volunteer.fetch("id").to_i
@@ -46,7 +46,7 @@ class Project
 
   def update(attributes)
     @title = attributes.fetch(:title, @title)
-    DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{self.id()};")
+    DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{self.id};")
   end
 
   def delete
