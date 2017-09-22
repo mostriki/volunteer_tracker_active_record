c0b7a405598bee43875818ffb18da14d2497be20
Riki Montgomery <mostriki820@gmail.com>
2017-09-22 16:01:14 -0700

    include 14 passing specs

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,26 +1,46 @@
-# class Volunteer
-#   attr_reader(:title, :id)
-#
-#   def initialize(attributes)
-#     @name = attributes[:name]
-#     @id = attributes[:id]
-#   end
-#
-# #passing
-#   def self.all
-#     all_volunteers = DB.exec("SELECT * FROM volunteers;")
-#     volunteers = []
-#     all_volunteers.each() do |volunteer|
-#       name = volunteer.fetch("name")
-#       id = name.fetch("id").to_i()
-#       volunteers.push(Volunteer.new({:name => title, :id => id}))
-#     end
-#     volunteers
-#   end
-#
-#   def save
-#     result = DB.exec("INSERT INTO volunteers (name) VALUES ('#{@name}') RETURNING id;")
-#     binding.pry
-#     @id = result.first().fetch("id").to_i()
-#   end
-# end
+class Volunteer
+  attr_reader(:name, :project_id, :id)
+
+  def initialize(attributes)
+    @name = attributes[:name]
+    @project_id = attributes[:project_id]
+    @id = attributes[:id]
+  end
+
+  def self.all
+    all_volunteers = DB.exec("SELECT * FROM volunteers;")
+    volunteers = []
+    all_volunteers.each() do |volunteer|
+      name = volunteer.fetch("name")
+      project_id = volunteer.fetch("project_id").to_i()
+      id = volunteer.fetch("id").to_i()
+      volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
+    end
+    volunteers
+  end
+
+  # def self.find(id)
+  #   result = DB.exec("SELECT * FROM volunteers WHERE id = #{id};")
+  #   name = result.first().fetch("name")
+  #   Volunteer.new({:name => name, :project_id => project_id, :id => id})
+  # end
+
+  def save
+    result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', '#{@project_id}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
+
+  def ==(another_volunteer)
+    self.name().==(another_volunteer.name()).&(self.project_id().==(another_volunteer.project_id()))
+  end
+
+  def self.find(id)
+    found_volunteer = nil
+    Volunteer.all().each() do |volunteer|
+      if volunteer.id().==(id)
+        found_volunteer = volunteer
+      end
+    end
+    found_volunteer
+  end
+end
