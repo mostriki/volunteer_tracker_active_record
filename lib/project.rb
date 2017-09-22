b5678ae60a3c66b971fb02e784cc3886491438a8
Riki Montgomery <mostriki820@gmail.com>
2017-09-22 12:46:44 -0700

    add 5 passing specs for projects.rb

--- lib/project.rb
+++ lib/project.rb
@@ -1,3 +1,43 @@
 class Project
+  attr_reader(:title, :id)
 
+  def initialize(attributes)
+    @title = attributes.fetch(:title)
+    @id = attributes.fetch(:id)
+  end
+
+  def ==(another_project)
+    self.title().==(another_project.title()).&(self.id().==(another_project.id()))
+  end
+
+  def self.all
+    all_projects = DB.exec("SELECT * FROM projects;")
+    projects = []
+    all_projects.each() do |title|
+      project = title.fetch("title")
+      id = title.fetch("id").to_i()
+      projects.push(Project.new({:title => title, :id => id}))
+    end
+    projects
+  end
+
+  def save
+    result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
+
+  def self.find(id)
+    result = DB.exec("SELECT * FROM projects WHERE id = #{id};")
+    title = result.first().fetch("title")
+    Project.new({:title => title, :id => id})
+  end
+
+  def update(attributes)
+    @title = attributes.fetch(:title, @title)
+    DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{self.id()};")
+  end
+
+  def delete
+    DB.exec("DELETE FROM projects WHERE id = #{self.id()};")
+  end
 end
--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,7 +1,26 @@
-class Volunteer
-  attr_reader(:title :id)
-
-  def initialize(attributes)
-    @name = attributes[:name]
-    @id = attributes[:id]
-end
+# class Volunteer
+#   attr_reader(:title, :id)
+#
+#   def initialize(attributes)
+#     @name = attributes[:name]
+#     @id = attributes[:id]
+#   end
+#
+# #passing
+#   def self.all
+#     all_volunteers = DB.exec("SELECT * FROM volunteers;")
+#     volunteers = []
+#     all_volunteers.each() do |volunteer|
+#       name = volunteer.fetch("name")
+#       id = name.fetch("id").to_i()
+#       volunteers.push(Volunteer.new({:name => title, :id => id}))
+#     end
+#     volunteers
+#   end
+#
+#   def save
+#     result = DB.exec("INSERT INTO volunteers (name) VALUES ('#{@name}') RETURNING id;")
+#     binding.pry
+#     @id = result.first().fetch("id").to_i()
+#   end
+# end
