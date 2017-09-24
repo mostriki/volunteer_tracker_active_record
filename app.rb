6669c44340def02af39afde2d48d5781e4e69782
Riki Montgomery <mostriki820@gmail.com>
2017-09-24 14:17:09 -0700

    Pass 20 spec tests

---
+++ app.rb
@@ -0,0 +1,40 @@
+require 'sinatra'
+require 'pry'
+require 'sinatra/reloader'
+also_reload '.lib/**/*.rb'
+require './lib/volunteer'
+require './lib/project'
+require 'pg'
+
+DB = PG.connect({dbname: 'volunteer_tracker'})
+
+get ('/') do
+  @projects = Project.all
+  erb(:index)
+end
+
+post('/projects') do
+  title = params['title']
+  project = Project.new({title: title, id: nil})
+  project.save
+  @projects = Project.all
+  erb(:index)
+end
+
+get("/projects/:id") do
+  @projects = Project.all
+  @project = Project.find(params.fetch("id").to_i())
+  erb(:project)
+end
+
+post("/projects/:id") do
+  name = params['name']
+  project_id = params[:id]
+
+  volunteer = Volunteer.new({name: name, project_id: project_id, id: nil})
+  volunteer.save
+
+  @projects = Project.all
+  @project = Project.find(params.fetch("id").to_i())
+  erb(:project)
+end
